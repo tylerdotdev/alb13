c62f0ab845f421a0d907872957b8f3328055a9a4
Tyler Anton <tylerdotdev@gmail.com>
2021-03-17 16:11:17 -0700

    abstract out event handlers

--- server/events.go
+++ server/events.go
@@ -8,38 +8,58 @@ import (
 	"antonlabs.io/alb13/websocket"
 )
 
+func handleSub(pool *websocket.Pool, event twitch.Event) {
+	message := websocket.BroadcastMessage{Event: event.Name, Data: event.SubEvent}
+	pool.Broadcast <- message
+}
+
+func handleResub(pool *websocket.Pool, event twitch.Event) {
+	message := websocket.BroadcastMessage{Event: event.Name, Data: event.ResubEvent}
+	pool.Broadcast <- message
+}
+
+func handleGiftSub(pool *websocket.Pool, event twitch.Event) {
+	message := websocket.BroadcastMessage{Event: event.Name, Data: event.GiftSubEvent}
+	pool.Broadcast <- message
+}
+
+func handleCheer(pool *websocket.Pool, event twitch.Event) {
+	if event.CheerEvent.Bits >= 100 {
+		message := websocket.BroadcastMessage{Event: event.Name, Data: event.CheerEvent}
+		pool.Broadcast <- message
+	}
+
+	if event.CheerEvent.Bits >= 500 && strings.Contains(event.CheerEvent.Message, "hydrate") {
+		message := websocket.BroadcastMessage{Event: "hydrate"}
+		pool.Broadcast <- message
+	}
+}
+
+func handleRedemption(pool *websocket.Pool, event twitch.Event) {
+	if event.NotificationEvent.Reward.Title == "Hydrate" {
+		log.Println("Hydrate redeemed")
+		message := websocket.BroadcastMessage{Event: "hydrate"}
+		pool.Broadcast <- message
+	}
+}
+
 func eventHandler(pool *websocket.Pool, event twitch.Event) {
 	log.Println(event.Name)
 	switch event.Name {
 	case twitch.SUB:
-		message := websocket.BroadcastMessage{Event: event.Name, Data: event.SubEvent}
-		pool.Broadcast <- message
+		handleSub(pool, event)
 		break
 	case twitch.RESUB:
-		message := websocket.BroadcastMessage{Event: event.Name, Data: event.ResubEvent}
-		pool.Broadcast <- message
+		handleResub(pool, event)
 		break
 	case twitch.GIFTSUB:
-		message := websocket.BroadcastMessage{Event: event.Name, Data: event.GiftSubEvent}
-		pool.Broadcast <- message
+		handleGiftSub(pool, event)
 		break
 	case twitch.CHEER:
-		if event.CheerEvent.Bits >= 100 {
-			message := websocket.BroadcastMessage{Event: event.Name, Data: event.CheerEvent}
-			pool.Broadcast <- message
-		}
-
-		if event.CheerEvent.Bits >= 500 && strings.Contains(event.CheerEvent.Message, "hydrate") {
-			message := websocket.BroadcastMessage{Event: "hydrate"}
-			pool.Broadcast <- message
-		}
+		handleCheer(pool, event)
 		break
 	case twitch.REDEMPTION:
-		if event.NotificationEvent.Reward.Title == "Hydrate" {
-			log.Println("Hydrate redeemed")
-			message := websocket.BroadcastMessage{Event: "hydrate"}
-			pool.Broadcast <- message
-		}
+		handleRedemption(pool, event)
 	default:
 		break
 	}
